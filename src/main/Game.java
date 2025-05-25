b289354b265868e01578dc5f3e39833c8aa6a3f9
CyberGhetto-25 <bridy.marc@gmail.com>
2025-05-25 10:30:25 +0200

    Supression de code non-fonctionnel

--- src/main/Game.java
+++ src/main/Game.java
@@ -94,20 +94,4 @@ public class Game {
         return commandManager;
     }
 
-//    public Game(){
-//        System.out.println("Initializing game...");
-//
-//        //String result = Array2Dprinter.print2DArray(Map, 1, 1);
-//        //System.out.println(result);
-//    }
-//
-//    public void run() {
-//        System.out.println("Running game...");
-//        // your runtime code here...
-//
-//
-//
-//        // end of game
-//    }
-
 }
